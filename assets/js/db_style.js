abf131ee3e1dfc8e2d690a39dd95a7fd244def23
Mamun Ahmed <mamunahmed678@gmail.com>
2019-09-27 18:45:01 +0600

    GetIn Highway Dashboard Home page

---
+++ assets/js/db_style.js
@@ -0,0 +1,36 @@
+$(function () {
+
+    'use strict';
+  
+    (function () {
+  
+      var aside = $('.cv_db_aside_area'),
+  
+          showAsideBtn = $('.cv_db_aside_nav_toggle_btn'),
+  
+          contents = $('#main_content');
+  
+      showAsideBtn.on("click", function () {
+  
+        aside.toggleClass('cv_db_show_aside_nav');
+        contents.toggleClass('cv_db_content_fullwidth');
+  
+      });
+  
+      if ($(window).width() <= 767) {
+  
+        aside.addClass('cv_db_show_aside_nav');
+  
+      }
+      $(window).on('resize', function () {
+  
+        if ($(window).width() > 767) {
+  
+          aside.removeClass('cv_db_show_aside_nav');
+  
+        }
+  
+      });
+      
+    }());
+});
