fe97622622dcf9a1da9fdb341f890c499e973add
Nipu <nipuchakraborty86@gmail.com>
2019-09-29 17:17:59 +0600

    fixed close btn

--- assets/js/db_style.js
+++ assets/js/db_style.js
@@ -8,7 +8,8 @@ $(function () {
   
           showAsideBtn = $('.cv_db_aside_nav_toggle_btn'),
   
-          contents = $('#main_content');
+          contents = $('#main_content'),
+          closeBtn=$('.closebtn');
   
           showAsideBtn.on("click", function () {
             
@@ -23,12 +24,24 @@ $(function () {
         aside.addClass('cv_db_show_aside_nav overlay active');
 
         contents.addClass('cv_db_content_fullwidth');
+
+        closeBtn.addClass('active')
+
       }
       $(window).on('resize', function () {
         if ($(window).width() > 767) {
           aside.removeClass('cv_db_show_aside_nav');
         }
       });
+
+   
       
     }());
 });
+
+$('.closebtn').on('click',function(){
+     $('aside').removeClass('cv_db_aside_area clearfix cv_db_full_height cv_db_scrollbar overlay active');
+     $('aside').addClass('cv_db_aside_area clearfix cv_db_full_height cv_db_scrollbar cv_db_show_aside_nav overlay active');
+})
+
+
